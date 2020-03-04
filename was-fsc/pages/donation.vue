d79607e61c53cd70a362750f5c779a10255c1deb
HyunBin_Jeon <peterhyun1234@gmail.com>
2020-03-04 20:02:54 +0900

    add donation page

---
+++ was-fsc/pages/donation.vue
@@ -0,0 +1,24 @@
+<template>
+    <v-app id="inspire">
+        Donation page
+    </v-app>
+</template>
+
+<script>
+
+export default {
+  components: {
+  }
+}
+</script>
+
+<style>
+.container {
+  min-height: 100vh;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  text-align: center;
+}
+</style>
+
