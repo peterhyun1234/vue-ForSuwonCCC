5c10a02f9ed5708372ae95f7e50eaf3418fb7b05
HyunBin_Jeon <peterhyun1234@gmail.com>
2020-03-05 18:23:44 +0900

    aafter fixing loader error

--- was-fsc/nuxt.config.js
+++ was-fsc/nuxt.config.js
@@ -27,10 +27,7 @@ module.exports = {
 
     buildModules: [
         // Simple usage
-        '@nuxtjs/vuetify',
-
-        // With options
-        ['@nuxtjs/vuetify', { /* module options */ }]
+        '@nuxtjs/vuetify'
     ],
 
 
