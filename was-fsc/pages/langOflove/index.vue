9c1d89319f4495d2e8002bca81be4881608df20f
HyunBin_Jeon <peterhyun1234@gmail.com>
2020-03-13 00:53:48 +0900

    after adopting language of love

--- was-fsc/pages/langOflove/index.vue
+++ was-fsc/pages/langOflove/index.vue
@@ -1,7 +1,35 @@
 <template>
   <v-app id="inspire">
     <v-container grid-list-xl>
-      사랑의 언어
+      <v-layout 
+        align-center 
+        justify-center
+        row 
+        wrap
+      >
+        <v-flex
+          xs12
+        >
+          <p>사랑의 언어에 대한 설명~~</p>
+        </v-flex>
+      </v-layout>
+      <v-layout 
+        align-center 
+        justify-center
+        row 
+        wrap
+      >
+        <v-flex
+          xs12
+        >
+          <v-btn 
+            text
+            to="/langOflove/test"
+          >
+            사랑의 언어 테스트 시작하기
+          </v-btn>
+        </v-flex>
+      </v-layout>
     </v-container>
   </v-app>
 </template>
--- was-fsc/pages/langOflove/test.vue
+++ was-fsc/pages/langOflove/test.vue
@@ -1,10 +1,61 @@
+
+
 <template>
   <v-app id="inspire">
-    <v-container grid-list-xl>
-      사랑의 언어 테스트
+    <v-container class="grey lighten-5">
+      <v-row>
+        <v-col>
+          <v-card>
+            One of two columns
+          </v-card>
+        </v-col>
+        <v-col>
+          <v-card>
+            One of two columns
+          </v-card>
+        </v-col>
+        <v-col>
+          <v-card>
+            One of two columns
+          </v-card>
+        </v-col>
+      </v-row>
+      <v-row>
+        <v-col>
+          <v-card>
+            One of two columns
+          </v-card>
+        </v-col>
+        <v-col>
+          <v-card>
+            One of two columns
+          </v-card>
+        </v-col>
+        <v-col>
+          <v-card>
+            One of two columns
+          </v-card>
+        </v-col>
+      </v-row>
+      <v-row>
+        <v-col
+          v-for="n in 3"
+          :key="n"
+          cols="sm"
+        >
+          <v-card
+            class="pa-2"
+            outlined
+            tile
+          >
+            col
+          </v-card>
+        </v-col>
+      </v-row>
     </v-container>
   </v-app>
 </template>
+
 
 <script>
 
