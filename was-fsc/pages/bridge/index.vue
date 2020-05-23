a5a972a93194a87f11719bfda3d9097c8a99f13f
HyunBin_Jeon <peterhyun1234@gmail.com>
2020-05-23 21:43:25 +0900

    add legend event

--- was-fsc/pages/bridge/index.vue
+++ was-fsc/pages/bridge/index.vue
@@ -1,9 +1,14 @@
 <template>
   <v-app id="inspire">
     <v-container grid-list-xl>
-        <ejs-accumulationchart id="container">
+        <ejs-accumulationchart id="container" :legendSettings='legendSettings'>
             <e-accumulation-series-collection>
-                <e-accumulation-series :dataSource='seriesData' xName='x' yName='y' :border='border'  :dataLabel='datalabel' :pointColorMapping=' pointColorMapping'> </e-accumulation-series>
+                <e-accumulation-series 
+                :dataSource='seriesData' 
+                xName='x' yName='y' 
+                :border='border'  
+                :dataLabel='datalabel'
+                :pointColorMapping=' pointColorMapping'> </e-accumulation-series>
             </e-accumulation-series-collection>
         </ejs-accumulationchart>
     </v-container>
@@ -12,28 +17,34 @@
 
 <script>
 import Vue from "vue";
-import { AccumulationChartPlugin, PieSeries, AccumulationDataLabel } from "@syncfusion/ej2-vue-charts";
+import { AccumulationChartPlugin, PieSeries, AccumulationDataLabel, AccumulationLegend } from "@syncfusion/ej2-vue-charts";
 
 Vue.use(AccumulationChartPlugin);
 
 export default {
   data() {
     return {
+      legendSettings:{ position:'Top' ,alignment:'Near'},
       seriesData: [
-                { x: 'Apr', y: 13.5, fill: '#81e2a1', text: 'April' }, 
-                { x: 'Jan', y: 3, fill: '#498fff', text:'January' }, 
-                { x: 'Feb', y: 3.5, fill: '#ffa060', text: 'February' },
-                { x: 'Mar', y: 7, fill: '#ff68b6', text: 'March' }, 
-            ],
-            border: {color: 'white', width: 2},
-            datalabel: {
-              visible: true, 
-              name: 'text', },
-            pointColorMapping: 'fill',
-        };
+          { x: 'Apr', y: 13.5, fill: '#81e2a1', text: 'April' }, 
+          { x: 'Jan', y: 3, fill: '#498fff', text:'January' }, 
+          { x: 'Feb', y: 3.5, fill: '#ffa060', text: 'February' },
+          { x: 'Mar', y: 7, fill: '#ff68b6', text: 'March' }, 
+      ],
+      border: {color: 'white', width: 2},
+      datalabel: {
+        visible: true,
+        position: 'Outside',
+        name: 'text', 
+        font: {color: "#ffa060", size: "16px" },
+        border: {color: 'black', width: 1},
+        fill: 'fill'
+      },
+      pointColorMapping: 'fill',
+    };
   },
   provide: {
-     accumulationchart: [PieSeries, AccumulationDataLabel]
+     accumulationchart: [PieSeries, AccumulationDataLabel, AccumulationLegend]
   }
 };
 </script>
