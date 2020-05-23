f4053863017d4d97036f96e80295b09ac89fe16e
HyunBin_Jeon <peterhyun1234@gmail.com>
2020-05-23 20:14:24 +0900

    delete existing chart

--- was-fsc/pages/langOflove/test.vue
+++ was-fsc/pages/langOflove/test.vue
@@ -113,11 +113,7 @@
                   <v-btn dark text @click="resultDialog = false">Save</v-btn>
                 </v-toolbar-items>
               </v-toolbar>
-              <ejs-accumulationchart id="container">
-                  <e-accumulation-series-collection>
-                      <e-accumulation-series :dataSource='seriesData' type='Pie' xName='x' yName='y'> </e-accumulation-series>
-                  </e-accumulation-series-collection>
-              </ejs-accumulationchart>
+<!--  -->
               <v-divider></v-divider>
               <v-list three-line subheader>
                 <v-subheader>General</v-subheader>
@@ -159,10 +155,6 @@
 
 
 <script>
-import { AccumulationChartPlugin, PieSeries } from "@syncfusion/ej2-charts";
- 
-Vue.use(AccumulationChartPlugin);
-
 const degreeOfQ = 30;
 
 export default {
@@ -362,14 +354,6 @@ export default {
         y: 0,
         mobile: false
       },
-      seriesData: [
-          { x: 'United States', y: 45, text: 'USA', fill: '#00226C' },
-          { x: 'Australia', y: 53, text: 'AUS: 14%', fill: '#0450C2' },
-          { x: 'China', y: 56, text: 'CHN', fill: '#0073DC' },
-          { x: 'India', y: 61, text: 'IND', fill: '#0D98FF' },
-          { x: 'Japan', y: 13, text: 'JPN', fill: '#9CD9FF' },
-          { x: 'United Kingdom', y: 71, text: 'UK', fill: '#0450C2' }
-      ],
     }
   },
 
