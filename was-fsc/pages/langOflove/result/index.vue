96d3221b867cc3b7a4073392dee1697435427315
HyunBin_Jeon <peterhyun1234@gmail.com>
2020-05-24 20:40:05 +0900

    add result page

---
+++ was-fsc/pages/langOflove/result/index.vue
@@ -0,0 +1,19 @@
+<template>
+  <v-app id="inspire">
+    <v-container grid-list-xl>
+      result
+    </v-container>
+  </v-app>
+</template>
+
+<script>
+
+export default {
+  components: {
+  },
+  data () {
+    return {
+    }
+  },
+}
+</script>
