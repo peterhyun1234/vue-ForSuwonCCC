a884323628b8a24a88448927c053486381a1eb23
HyunBin_Jeon <peterhyun1234@gmail.com>
2020-05-25 19:44:12 +0900

    pie chart testing and add percentage

--- was-fsc/pages/bridge/index.vue
+++ was-fsc/pages/bridge/index.vue
@@ -1,14 +1,12 @@
 <template>
   <v-app id="inspire">
     <v-container grid-list-xl>
-        <ejs-accumulationchart id="container" :legendSettings='legendSettings' :tooltip='tooltip'>
+        <ejs-accumulationchart id="container">
             <e-accumulation-series-collection>
-                <e-accumulation-series 
-                :dataSource='seriesData' 
-                xName='x' yName='y' 
-                :border='border'  
-                :dataLabel='datalabel'
-                :pointColorMapping=' pointColorMapping'> </e-accumulation-series>
+                <e-accumulation-series :dataSource='seriesData' 
+                xName='x' yName='y' :dataLabel='datalabel'  
+                :pointColorMapping=' pointColorMapping'> 
+                </e-accumulation-series>
             </e-accumulation-series-collection>
         </ejs-accumulationchart>
     </v-container>
@@ -30,15 +28,17 @@ export default {
         shapeHeight: 25, shapeWidth: 25,
       },
       seriesData: [
-          { x: 'Apr', y: 13.5, fill: '#81e2a1', text: 'April' }, 
-          { x: 'Jan', y: 3, fill: '#498fff', text:'January' }, 
-          { x: 'Feb', y: 3.5, fill: '#ffa060', text: 'February' },
-          { x: 'Mar', y: 7, fill: '#ff68b6', text: 'March' }, 
+          { x: 'Apr', y: 13.5, fill: '#81e2a1', text: 'April', percentage: "40%"}, 
+          { x: 'Jan', y: 3, fill: '#498fff', text:'January', percentage: "25%" }, 
+          { x: 'Feb', y: 3.5, fill: '#ffa060', text: 'February', percentage: "20%" },
+          { x: 'Mar', y: 7, fill: '#ff68b6', text: 'March', percentage: "15%" }, 
       ],
       border: {color: 'white', width: 2},
-      tooltip:{
-        enable: true, 
-        format: '${series.name} ${point.x} : ${point.y}',
+      datalabel: {
+        visible: true,
+        position: 'Inside',
+        name: 'percentage',
+        font: {color: "white", size: "16px" },
       },
       pointColorMapping: 'fill',
     };
--- was-fsc/pages/langOflove/test/result.vue
+++ was-fsc/pages/langOflove/test/result.vue
@@ -7,6 +7,7 @@
               :dataSource='seriesData' 
               xName='x' yName='y' 
               :border='border'
+              :dataLabel='datalabel'
               :pointColorMapping=' pointColorMapping'> </e-accumulation-series>
           </e-accumulation-series-collection>
       </ejs-accumulationchart>
@@ -46,31 +47,41 @@ export default {
   data() {
     return {
       legendSettings:{ 
-        position:'Bottom' ,
+        position:'Top' ,
         alignment:'Center',
         shapeHeight: 25, shapeWidth: 25,
       },
       seriesData: [
           { x: '인정하는 말', y: 1, fill: '#f8ff69', text: 'A', 
+            percentage: "",
             icon: 'mdi-email-send-outline', 
-            description: "인간의 가장 큰 욕구중 하나는 누군가로부터 인정받고 싶은 욕구입니다. 잘한 일을 칭찬해보세요! 우리는 잘한 일이 있어도 말없이 넘어가기 쉽습니다. 그러나 구체적으로 칭찬하는 것이 중요합니다."}, 
+            description: "사랑을 표현하는 방법이 격려의 말, 인정해 주는 말입니다. 칭찬하는 말이나 감사의 표현, 그 사람을 인정하고 높혀주는 말들이 이 사람에겐 행복이고, 큰 힘을 줍니다. 또한 이 사람은 사랑의 감정을 말로 전달하고 싶어하며 온화한 말투를 좋아하고, 명령하는 말투를 극히 싫어합니다. \"미안해 , 고마워, 정말 잘하는구나, 사랑해, 넌 정말 좋은 사람이야\" 와 같은 사과, 칭찬, 격려, 애정의 말들을 배우자, 또는 주변사람들로 부터 들었을때 \"내가 사랑받고 있구나\" 라고 느끼며 매사에 최선을 다하게 됩니다."}, 
           { x: '함께하는 시간', y: 1, fill: '#498fff', text: 'B', 
+            percentage: "",
             icon: 'mdi-email-send-outline', 
             description: "함께 하는 시간이란 누군가에게 온전히 관심을 집중시키는 것을 의미합니다. TV를 끄고 소파에 마주 앉아 대화를 하면서 서로에게 관심을 집중하는 것과 같습니다. 산책을 하며 외식을 할 때도 서로 마주 보며 대화를 하는 것이 중요합니다." }, 
           { x: '선물', y: 1, fill: '#ffa060', text: 'C', 
             icon: 'mdi-email-send-outline', 
             description: "선물은 상징일 수 있지만 선물을 준 사람과 그 때의 느낌은 오래오래 기억됩니다. 마음이 중요하다고 생각하여 선물하지 않으면 나중에는 마음까지 멀어질 수 있습니다. 지금 생각나는 소중한 사람들에게 선물을 전달해보세요!" },
-          { x: '봉사(헌신)', y: 1, fill: '#69ffb1', text: 'D', 
+          { x: '봉사의 손길', y: 1, fill: '#69ffb1', text: 'D', 
+            percentage: "",
             icon: 'mdi-email-send-outline', 
             description: "상대를 도와줌으로써 기쁘게 하고 무엇인가를 함으로써 당신의 사랑을 표현하는 것을 말합니다. " }, 
           { x: '스킨쉽', y: 1, fill: '#ff68b6', text: 'E', 
+            percentage: "",
             icon: 'mdi-email-send-outline', 
             description: "육체적인 접촉을 전혀 갖지 않고 지낸 아이들보다 안아주거나 키스를 해 준 아이들이 훨씬 건강하게 자란다고 합니다." }, 
       ],
-      border: {color: 'white', width: 2},
+      border: {color: 'white', width: 3},
       tooltip:{
         enable: true, 
         format: '${series.name} ${point.x} : ${point.y}',
+      },
+      datalabel: {
+        visible: true,
+        position: 'Inside',
+        name: 'percentage',
+        font: {color: "white", size: "16px" },
       },
       pointColorMapping: 'fill',
     };
@@ -94,6 +105,15 @@ export default {
       this.seriesData[2].y = parsedResult.C;
       this.seriesData[3].y = parsedResult.D;
       this.seriesData[4].y = parsedResult.E;
+
+      //console.log(parsedResult.A + "/" + "30 = " + parsedResult.A/30);
+
+      this.seriesData[0].percentage = (((parsedResult.A)/30)*100).toFixed(2) + "%"
+      this.seriesData[1].percentage = (((parsedResult.B)/30)*100).toFixed(2) + "%"
+      this.seriesData[2].percentage = (((parsedResult.C)/30)*100).toFixed(2) + "%"
+      this.seriesData[3].percentage = (((parsedResult.D)/30)*100).toFixed(2) + "%"
+      this.seriesData[4].percentage = (((parsedResult.E)/30)*100).toFixed(2) + "%"
+
 
       // sorting based on y value
       this.seriesData.sort(function(a, b){
