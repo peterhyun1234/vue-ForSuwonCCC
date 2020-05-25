394ab6113df3fd56f31b8043b5f4cbf710f99cdd
HyunBin_Jeon <peterhyun1234@gmail.com>
2020-05-25 20:14:41 +0900

    pie test

--- was-fsc/pages/bridge/index.vue
+++ was-fsc/pages/bridge/index.vue
@@ -1,51 +1,47 @@
 <template>
-  <v-app id="inspire">
-    <v-container grid-list-xl>
-        <ejs-accumulationchart id="container">
+    <div id="app">
+        <ejs-accumulationchart id="container" ref="pie" style='display:block;' :theme='theme' :legendSettings="legendSettings" :tooltip="tooltip" :enableAnimation='enableAnimation' :enableSmartLabels='enableSmartLabels' >
             <e-accumulation-series-collection>
-                <e-accumulation-series :dataSource='seriesData' 
-                xName='x' yName='y' :dataLabel='datalabel'  
-                :pointColorMapping=' pointColorMapping'> 
-                </e-accumulation-series>
+                <e-accumulation-series  :dataSource='data' xName='x' yName='y' :radius='radius' innerRadius="20%" :dataLabel="dataLabel"> </e-accumulation-series>
             </e-accumulation-series-collection>
         </ejs-accumulationchart>
-    </v-container>
-  </v-app>
+    </div>
 </template>
-
 <script>
 import Vue from "vue";
-import { AccumulationChartPlugin, PieSeries, AccumulationDataLabel, AccumulationLegend, AccumulationTooltip } from "@syncfusion/ej2-vue-charts";
+import { AccumulationChartPlugin, PieSeries } from "@syncfusion/ej2-vue-charts";
 
 Vue.use(AccumulationChartPlugin);
 
 export default {
   data() {
     return {
-      legendSettings:{ 
-        position:'Bottom' ,
-        alignment:'Center',
-        shapeHeight: 25, shapeWidth: 25,
-      },
-      seriesData: [
-          { x: 'Apr', y: 13.5, fill: '#81e2a1', text: 'April', percentage: "40%"}, 
-          { x: 'Jan', y: 3, fill: '#498fff', text:'January', percentage: "25%" }, 
-          { x: 'Feb', y: 3.5, fill: '#ffa060', text: 'February', percentage: "20%" },
-          { x: 'Mar', y: 7, fill: '#ff68b6', text: 'March', percentage: "15%" }, 
-      ],
-      border: {color: 'white', width: 2},
-      datalabel: {
-        visible: true,
-        position: 'Inside',
-        name: 'percentage',
-        font: {color: "white", size: "16px" },
-      },
-      pointColorMapping: 'fill',
+      data:[
+        { x: 'Argentina', y: 505370, r: '150' },
+        { x: 'Belgium', y: 551500, r: '150' },
+        { x: 'Cuba', y: 312685, r: '150.6' },
+        { x: 'Dominican Republic', y: 350000, r: '145.5' },
+        { x: 'Egypt', y: 301000, r: '150.8' },
+        { x: 'Kazakhstan', y: 300000, r: '150.5' },
+        { x: 'Somalia', y: 357022, r: '150.6' }
+     ],
+     radius: 'r',
+     legendSettings: { visible: true },
+     dataLabel: { visible: true, position: 'Outside', name: 'x'},
+     tooltip: {
+        enable: true, header: '<b>${point.x}</b>', format: 'Composition: <b>${point.y}</b>'
+     },
+     enableAnimation: true,
+     enableSmartLabels: true,
     };
   },
   provide: {
-     accumulationchart: [PieSeries, AccumulationDataLabel, AccumulationLegend, AccumulationTooltip]
+     accumulationchart: [PieSeries]
   }
 };
 </script>
-
+<style>
+ #container {
+     height: 350px;
+ }
+</style>
